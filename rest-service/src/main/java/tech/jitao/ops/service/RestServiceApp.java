2bc3e355af8dba07c794ad6e4554550838c21862
Ji Tao <flmnix@gmail.com>
2021-09-12 22:43:37 +0800

    ~

--- rest-service/src/main/java/tech/jitao/ops/service/RestServiceApp.java
+++ rest-service/src/main/java/tech/jitao/ops/service/RestServiceApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServiceApp {
+public class RestServiceApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceApp.class, args);
+        SpringApplication.run(RestServiceApp.class, args);
     }
 }
